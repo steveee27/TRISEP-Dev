76b0a4a47dade84aa6812304a642009145d7362d
Marvel Martawidjaja <100202616+marvelm69@users.noreply.github.com>
2024-09-27 22:46:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -671,10 +671,10 @@ elif page == '📚 Step 3: Grow':
                     st.session_state.course_page += 1
 
     # Add the new button for adding your own course
-    st.markdown("---")
-    st.markdown("### 🌟 Can't find what you're looking for?")
-    if st.button("➕ Add Your Own Course"):
-        st.markdown("[Click here to add your course](https://docs.google.com/forms/d/e/1FAIpQLSedcvWeMfGsXcaoLO7lZ1MKi_EZq8fAimxlH7sxZAKZgNcslQ/viewform)", unsafe_allow_html=True)
+st.markdown("---")
+st.markdown("### 🌟 Can't find what you're looking for?")
+if st.button("➕ Add Your Own Course"):
+    st.markdown("[Click here to add your course](https://docs.google.com/forms/d/e/1FAIpQLSedcvWeMfGsXcaoLO7lZ1MKi_EZq8fAimxlH7sxZAKZgNcslQ/viewform)", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     pass
