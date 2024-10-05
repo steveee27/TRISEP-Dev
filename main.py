e5f2867d26ad9a0af251b428affa268df3735c7e
Marvel Martawidjaja <100202616+marvelm69@users.noreply.github.com>
2024-10-05 08:10:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -83,6 +83,9 @@ def load_job_data():
     csv_url = 'https://docs.google.com/spreadsheets/d/1huKbxP4W5c5sBWAQ5LzerhdId6TR9glCRFKn7DNOKEE/export?format=csv&gid=1980208131'
     df_job = pd.read_csv(csv_url, on_bad_lines='skip', engine='python')
     
+    # Remove duplicates based on 'job_posting_url'
+    df_job = df_job.drop_duplicates(subset=['job_posting_url'], keep='first')
+    
     df_job['Combined'] = df_job['title'].fillna('') + ' ' + df_job['description_x'].fillna('') + ' ' + df_job['skills_desc'].fillna('')
     df_job['Combined'] = df_job['Combined'].apply(preprocess_text_simple)
     df_job['title'] = df_job['title'].apply(remove_asterisks)
